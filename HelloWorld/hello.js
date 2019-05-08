c6255a24a16185bb8f34253cc0429be199d4e30d
swapz17 <swapz17@github.com>
2019-05-08 17:29:35 +0530

    Add web page

--- HelloWorld/hello.js
+++ HelloWorld/hello.js
@@ -2,3 +2,7 @@
 // Test code
 console.log("Hello TypeScript");
 console.log("Hello Git");
+window.onload = function () {
+    console.log("hello vscode");
+};
+//# sourceMappingURL=hello.js.map
--- HelloWorld/hello.ts
+++ HelloWorld/hello.ts
@@ -1,3 +1,7 @@
 // Test code
 console.log("Hello TypeScript");
 console.log("Hello Git");
+
+window.onload = () => { 
+    console.log("hello vscode"); 
+};
